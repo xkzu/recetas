e528573cc4a8e15ad8746a06c8cd4599a3a9639f
Kzu <alvaro.vega.pavez@gmail.com>
2024-12-09 03:58:14 -0300

    update: interceptor

--- src/main/java/cl/duoc/app/recetas/config/RestTemplateConfig.java
+++ src/main/java/cl/duoc/app/recetas/config/RestTemplateConfig.java
@@ -1,13 +1,33 @@
 package cl.duoc.app.recetas.config;
 
+import cl.duoc.app.recetas.interceptor.JwtRequestInterceptor;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
+
+    private final HttpSession session;
+
+    public RestTemplateConfig(HttpSession session) {
+        this.session = session;
+    }
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Configurar el interceptor para agregar el token JWT
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new JwtRequestInterceptor(session));
+        restTemplate.setInterceptors(interceptors);
+
+        return restTemplate;
     }
 }
